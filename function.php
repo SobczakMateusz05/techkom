0487dd1780a0202b60fa28f7a51aa7e367149cb5
SkylLend27 <sobczak.mateusz27@gmail.com>
2024-02-07 23:19:49 +0100

    php function

---
+++ function.php
@@ -0,0 +1,11 @@
+<?php 
+Function products($conn){
+    
+    $sql="SELECT nazwa from type";
+    if($result=$conn->query($sql)){
+        while($row=$result->fetch_assoc()){
+            echo '<li class="products disable disable-selection"><a href="#" onclick="category('."'". $row["nazwa"]. "'". ')">'. UCWORDS($row["nazwa"]).'</a></li>';
+        }
+    }
+}
+?>
