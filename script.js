09ebc6f048e8be11ae10d80822960a26a511ca33
SkylLend27 <sobczak.mateusz27@gmail.com>
2024-02-07 13:20:18 +0100

    script

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
-function products(){
-    var product = document.getElementsByClassName("products");
-    for (let i = 0; i < product.length; i++) {
-        const element = product[i];
-        element.classList.toggle("disable");
-    }
-}
-function usluga(){
-    var usluga = document.getElementsByClassName("usluga");
-    for (let i = 0; i < usluga.length; i++) {
-        const element = usluga[i];
-        element.classList.toggle("disable");
-    }
-}
-function actual(){
-    var actual = document.getElementsByClassName("actual");
-    for (let i = 0; i < actual.length; i++) {
-        const element = actual[i];
+function list(a){
+    var item = document.getElementsByClassName(a);
+    for (let i = 0; i < item.length; i++) {
+        const element = item[i];
         element.classList.toggle("disable");
     }
 }
@@ -23,9 +9,8 @@ function filtr(){
     var filtr = document.getElementsByClassName("filtr")[0];
     filtr.classList.toggle("disable");
 }
-function sort(){
-    var sort = document.getElementsByClassName("sort")[0];
-    sort.classList.toggle("disable");
+function category(a){
+    window.location.href= "ofert.php?category=" +a;
 }
 
 
