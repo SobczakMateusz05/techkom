1e2ccd3c5bd08470e623cca131b3aa55a7408d3b
SkylLend27 <sobczak.mateusz27@gmail.com>
2024-02-09 19:41:34 +0100

    function

--- function.php
+++ function.php
@@ -7,4 +7,22 @@ Function products($conn){
         }
     }
 }
+Function options($a){
+    for ($i=0; $i < 6; $i++) { 
+        echo '<option value="'.$i.'"';
+    if($a==$i){
+        echo 'selected="selected"';
+    }
+    echo '>'.$i.'</option>';
+    } 
+}
+Function cartitem($image, $name, $price, $amount, $id, $suma){
+    echo '<div class="element"><div class="product"><img src="data:image/jpg;charset=utf8;base64,'.base64_encode($image).'" /><h3>';
+    echo $name. '</h3></div><div class="options"><h3 class="price">'. $price. ' zł';
+    echo '</h3> <form method="POST" action="cart.html"><select name="id" class="amount" onchange="this.form.submit()">';
+    options($amount);
+    echo '</select><noscript><input type="submit" name="submit" value="sort"></noscript></form>';
+    echo '<h3 class="delete disable-selection" onclick="operation('.$id. " ,'del')";
+    echo '">USUŃ</h3></div></div><div class="sum"><h3>Łączna kwota: '.$suma.' zł</h3></div>';
+}
 ?>
